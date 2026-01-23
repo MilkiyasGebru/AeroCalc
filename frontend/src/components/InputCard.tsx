38e7ee17da162c9386c489f3419da48d2ce6aa81
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-01-23 11:09:38 +0300

    Now the Input componets is multiple page

--- frontend/src/components/InputCard.tsx
+++ frontend/src/components/InputCard.tsx
@@ -3,6 +3,10 @@ import {Calculator} from "lucide-react";
 import {Input} from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {Button} from "@/components/ui/button.tsx";
+import {useState} from "react";
+import {Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue,
+} from "@/components/ui/select"
+import MeanSpeedGraph from "@/components/MeanSpeedGraph.tsx";
 
 interface InputCardProps {
     width: number;
@@ -30,148 +34,233 @@ interface InputCardProps {
     handleClick: ()=> void;
 }
 
+
+
 export default function InputCard(props: InputCardProps){
+    const [step, setStep] = useState(0);
+    const [terrain, setTerrain] = useState<string>("open")
+    const coefficient = (terrain == "open")? (props.height/10)**0.28: 0.5*((props.height/12.7)**0.5);
+    const MaxHeight: number = props.height * 1.5;
+    const graph_data = [];
+    let h : number = 0;
+    while (h < MaxHeight){
+        const c = (terrain == "open")? (h/10)**0.28: 0.5*((h/12.7)**0.5);
+        graph_data.push({
+            height: h,
+            speed: props.meanSpeed * c ** 0.5
+        })
+        h += 1;
+    }
+    const handleNext = ()=>{
+        setStep(s => s + 1)
+    }
+    const handlePrev = ()=>{
+        setStep(s => s - 1)
+    }
     return (
         <Card className="  bg-white border-transparent mb-4">
-            <CardHeader>
-                <CardTitle className="flex gap-2 items-center text-xl">
-                    <Calculator className="w-5 h-5 text-blue-300"/>
-                    Input Parameters
-                </CardTitle>
-                <CardDescription>
-                    Enter building dimensions and wind parameters
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-4">
-                <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
-                    <div className="space-y-2">
-                        <Label htmlFor="width">Width (m)</Label>
-                        <Input
-                            id="width"
-                            type="number"
-                            value={props.width}
-                            onChange={(e) => props.setWidth(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="height">Height (m)</Label>
-                        <Input
-                            id="height"
-                            type="number"
-                            value={props.height}
-                            onChange={(e) => props.setHeight(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="depth">Depth (m)</Label>
-                        <Input
-                            id="depth"
-                            type="number"
-                            value={props.depth}
-                            onChange={(e) => props.setDepth(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="mean_velocity">Mean Velocity (m/s)</Label>
-                        <Input
-                            id="mean_velocity"
-                            type="number"
-                            value={props.meanSpeed}
-                            onChange={(e) => props.setMeanSpeed(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
+            {step == 0 && <>
+                <CardHeader>
+                    <CardTitle className="flex gap-2 items-center text-xl">
+                        <Calculator className="w-5 h-5 text-blue-300"/>
+                        Geometry Input Parameters
+                    </CardTitle>
+                    <CardDescription>
+                        Enter building geometery dimensions
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    <div className="grid grid-cols sm:grid-cols-2 gap-2">
+                        <div className="space-y-2">
+                            <Label htmlFor="width">Width (m)</Label>
+                            <Input
+                                id="width"
+                                type="number"
+                                value={props.width}
+                                onChange={(e) => props.setWidth(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="height">Height (m)</Label>
+                            <Input
+                                id="height"
+                                type="number"
+                                value={props.height}
+                                onChange={(e) => props.setHeight(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="depth">Depth (m)</Label>
+                            <Input
+                                id="depth"
+                                type="number"
+                                value={props.depth}
+                                onChange={(e) => props.setDepth(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="NFloors">Number of Floors</Label>
+                            <Input
+                                id="NFloors"
+                                type="number"
+                                value={props.totalFloors}
+                                onChange={(e) => props.setTotalFloors(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+
                     </div>
 
-                    <div className="space-y-2">
-                        <Label htmlFor="NFloors">Building Density</Label>
-                        <Input
-                            id="NFloors"
-                            type="number"
-                            value={props.buildingDensity}
-                            onChange={(e) => props.setBuildingDensity(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
+                    <div className="flex gap-2">
+                        <Button
+                            className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
+                            onClick={handlePrev}
+                            disabled={step == 0}
+                        >
+                            Prev
+                        </Button>
+                        <Button
+                            className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
+                            onClick={handleNext}
+                        >
+                            Next
+                        </Button>
+
                     </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="NFloors">Number of Floors</Label>
-                        <Input
-                            id="NFloors"
-                            type="number"
-                            value={props.totalFloors}
-                            onChange={(e) => props.setTotalFloors(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
+                </CardContent>
+            </>}
+            {step == 1 && <>
+                <CardHeader>
+                    <CardTitle className="flex gap-2 items-center text-xl">
+                        <Calculator className="w-5 h-5 text-blue-300"/>
+                        Dynamic Property Input Parameters
+                    </CardTitle>
+                    <CardDescription>
+                        Enter the dynamic properites
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    <div className="grid grid-cols sm:grid-cols-2 gap-2">
+
+                        <div className="space-y-2">
+                            <Label htmlFor="NFloors">Building Density</Label>
+                            <Input
+                                id="NFloors"
+                                type="number"
+                                value={props.buildingDensity}
+                                onChange={(e) => props.setBuildingDensity(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+
+                        <div className="space-y-2">
+                            <Label htmlFor="damping">Damping</Label>
+                            <Input
+                                id="damping"
+                                type="number"
+                                value={props.damping}
+                                onChange={(e) => props.setDamping(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="Fundamental Period">Tone</Label>
+                            <Input
+                                id="Tone"
+                                type="number"
+                                value={props.Tone}
+                                onChange={(e) => props.setTone(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
+                            />
+                        </div>
+
                     </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="damping">Damping</Label>
-                        <Input
-                            id="damping"
-                            type="number"
-                            value={props.damping}
-                            onChange={(e) => props.setDamping(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="Tone">Tone</Label>
-                        <Input
-                            id="Tone"
-                            type="number"
-                            value={props.Tone}
-                            onChange={(e) => props.setTone(parseFloat(e.target.value))}
-                            className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
-                        />
+                    <div className="flex gap-2">
+                        <Button
+                            className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
+                            onClick={handlePrev}
+                            disabled={step == 0}
+                        >
+                            Prev
+                        </Button>
+                        <Button
+                            className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
+                            onClick={handleNext}
+                        >
+                            Next
+                        </Button>
+
                     </div>
 
-                </div>
-                <div className="border-t border-gray-200">
-                    <h4 className="text-sm font-medium text-muted-foreground mb-3">Frequency Range</h4>
-                    <div className="grid sm:grid-cols-3 gap-4">
+                </CardContent>
+            </>}
+            {step == 2 && <>
+                <CardHeader>
+                    <CardTitle className="flex gap-2 items-center text-xl">
+                        <Calculator className="w-5 h-5 text-blue-300"/>
+                        Wind Parameters
+                    </CardTitle>
+                    <CardDescription>
+                        Enter the wind properites
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    <div className="grid grid-cols sm:grid-cols-2 gap-2">
+
+
                         <div className="space-y-2">
-                            <Label htmlFor="initialFrequency">Initial (Hz)</Label>
+                            <Label htmlFor="mean_velocity">Mean Speed at 10meters at 10 years (m/s)</Label>
                             <Input
-                                id="initialFrequency"
+                                id="mean_velocity"
                                 type="number"
-                                step="0.01"
-                                value={props.initialFrequency}
-                                onChange={(e) =>props.setInitialFrequency(parseFloat(e.target.value))}
-                                className="font-mono bg-[hsl(210,20%,98%)]"
+                                value={props.meanSpeed}
+                                onChange={(e) => props.setMeanSpeed(parseFloat(e.target.value))}
+                                className="font-mono bg-[hsl(210,20%,98%)] border-transparent w-5/6 md:w-full"
                             />
                         </div>
-                        <div className="space-y-2">
-                            <Label htmlFor="finalFrequency">Final (Hz)</Label>
-                            <Input
-                                id="finalFrequency"
-                                type="number"
-                                step="0.01"
-                                value={props.finalFrequency}
-                                onChange={(e) =>props.setFinalFrequency(parseFloat(e.target.value))}
-                                className="font-mono bg-[hsl(210,20%,98%)]"
-                            />
+                        <div>
+                            <Label htmlFor="terrain">Terrain Type</Label>
+                            <Select value={terrain} onValueChange={setTerrain}>
+                                <SelectTrigger id="terrain" className="w-full max-w-48 border-transparent bg-[hsl(210,20%,98%)]">
+                                    <SelectValue placeholder="Open" />
+                                </SelectTrigger>
+                                <SelectContent className="bg-[hsl(210,20%,98%)]">
+                                    <SelectGroup>
+                                        <SelectLabel>Terrain</SelectLabel>
+                                        <SelectItem value="open" className="hover:bg-white">open</SelectItem>
+                                        <SelectItem value="rough" className="hover:bg-white">rough</SelectItem>
+
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
                         </div>
-                        <div className="space-y-2">
-                            <Label htmlFor="deltaFrequency">Delta (Hz)</Label>
-                            <Input
-                                id="deltaFrequency"
-                                type="number"
-                                step="0.01"
-                                value={props.deltaFrequency}
-                                onChange={(e) => props.setDeltaFrequency(parseFloat(e.target.value))}
-                                className="font-mono bg-[hsl(210,20%,98%)]"
-                            />
-                        </div>
+
                     </div>
-                </div>
-                <Button className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
-                    onClick={props.handleClick}
-                >
-                    <Calculator />
-                    Calculate
-                </Button>
-            </CardContent>
+                    <MeanSpeedGraph graph_data={graph_data} current_point={{height: props.height, speed: props.meanSpeed * coefficient ** 0.5}}/>
+
+
+                    <div className="flex gap-2">
+                        <Button
+                            className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
+                            onClick={handlePrev}
+                            disabled={step == 0}
+                        >
+                            Prev
+                        </Button>
+                        <Button
+                            className="w-full text-center flex justify-center hover:cursor-pointer bg-blue-400 text-white"
+                            onClick={handleNext}
+                        >
+                            Next
+                        </Button>
+
+                    </div>
+
+                </CardContent>
+            </>}
         </Card>
     )
 }
