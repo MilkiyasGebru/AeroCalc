d6adda2e2707974b7b9ceb459167e2cbe2a92af0
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-01-24 23:45:06 +0300

    Cleaned up

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {InputBuildingContextProvider} from "@/providers/InputBuildingContextProvider.tsx";
+import {OutputBuildingContextProvider} from "@/providers/OutputBuildingContextProvider.tsx";
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-    <App />
+      <InputBuildingContextProvider>
+        <OutputBuildingContextProvider>
+            <App />
+        </OutputBuildingContextProvider>
+      </InputBuildingContextProvider>
   </StrictMode>,
 )
