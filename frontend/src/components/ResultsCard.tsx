3620356db2aa7c1c7f4d9fd8e0ef032d42115138
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-02-11 19:16:45 +0300

    Made the resutls into a table

--- frontend/src/components/ResultsCard.tsx
+++ frontend/src/components/ResultsCard.tsx
@@ -1,53 +1,87 @@
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card.tsx";
-import {TrendingUp} from "lucide-react";
+
 import {useOutputBuildingContext} from "@/contexts/useOutputBuildingContext.ts";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 
 
 export default function ResultsCard() {
     const {ar, vr, experimentalAr, experimentalVr, accelartionYDirection} = useOutputBuildingContext()
+
     return (
         <>
-        { (ar != null || vr != null || experimentalAr != null || experimentalVr != null) && <Card className="bg-white border-transparent h-fit">
-            <CardHeader>
-                <CardTitle className="flex items-center gap-2 text-lg ">
-                    <TrendingUp className="w-5 h-5 text-blue-300"/>
-                    Calculation Results
-                </CardTitle>
-                <CardDescription>
-                    Enter parameters and click Calculate
-                </CardDescription>
+        {/*{ (ar != null || vr != null || experimentalAr != null || experimentalVr != null) && <Card className="bg-white border-transparent h-fit">*/}
+        {/*    <CardHeader>*/}
+        {/*        <CardTitle className="flex items-center gap-2 text-lg ">*/}
+        {/*            <TrendingUp className="w-5 h-5 text-blue-300"/>*/}
+        {/*            Calculation Results*/}
+        {/*        </CardTitle>*/}
+        {/*        <CardDescription>*/}
+        {/*            Enter parameters and click Calculate*/}
+        {/*        </CardDescription>*/}
 
-            </CardHeader>
-            <CardContent className="grid grid-cols sm:grid-cols-3 gap-2">
-                <div
-                    className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">
-                    <span className="font-bold">Analytical AR</span>
-                    <span>{ar ? ar.toFixed(3) : "?"}</span>
-                </div>
-                <div
-                    className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">
-                    <span className="font-bold">Analytical VR</span>
-                    <span>{vr ? vr.toFixed(3) : "?"}</span>
-                </div>
-                <div
-                    className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">
-                    <span className="font-semibold">Analytical Accelartion in Y-direction</span>
-                    <span>{accelartionYDirection ? accelartionYDirection.toFixed(3) : "?"}</span>
-                </div>
-                <div
-                    className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">
-                    <span className="font-bold">Experimental AR</span>
-                    <span>{experimentalAr ? experimentalAr.toFixed(3) : "?"}</span>
-                </div>
-                <div
-                    className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">
-                    <span className="font-bold">Experimental VR</span>
-                    <span>{experimentalVr ? experimentalVr.toFixed(3) : "?"}</span>
-                </div>
-            </CardContent>
-        </Card>
-        }
+        {/*    </CardHeader>*/}
+        {/*    <CardContent className="grid grid-cols sm:grid-cols-3 gap-2">*/}
+        {/*        <div*/}
+        {/*            className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">*/}
+        {/*            <span className="font-bold">Analytical AR</span>*/}
+        {/*            <span>{ar ? ar.toFixed(3) : "?"}</span>*/}
+        {/*        </div>*/}
+        {/*        <div*/}
+        {/*            className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">*/}
+        {/*            <span className="font-bold">Analytical VR</span>*/}
+        {/*            <span>{vr ? vr.toFixed(3) : "?"}</span>*/}
+        {/*        </div>*/}
+        {/*        <div*/}
+        {/*            className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">*/}
+        {/*            <span className="font-semibold">Analytical Accelartion in Y-direction</span>*/}
+        {/*            <span>{accelartionYDirection ? accelartionYDirection.toFixed(3) : "?"}</span>*/}
+        {/*        </div>*/}
+        {/*        <div*/}
+        {/*            className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">*/}
+        {/*            <span className="font-bold">Experimental AR</span>*/}
+        {/*            <span>{experimentalAr ? experimentalAr.toFixed(3) : "?"}</span>*/}
+        {/*        </div>*/}
+        {/*        <div*/}
+        {/*            className="flex flex-col border p-3 rounded-md items-center bg-[hsl(210,20%,98%)] border-transparent">*/}
+        {/*            <span className="font-bold">Experimental VR</span>*/}
+        {/*            <span>{experimentalVr ? experimentalVr.toFixed(3) : "?"}</span>*/}
+        {/*        </div>*/}
+        {/*    </CardContent>*/}
+        {/*</Card>*/}
+        {/*}*/}
+        <Table>
+            <TableCaption>Building dynamic response</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[100px]">   </TableHead>
+                    <TableHead>Across-wind response rms accelaration (milli-g)</TableHead>
+                    <TableHead>Along-wind response rms accelaration (milli-g)</TableHead>
+                    <TableHead className="text-right">Torsion response rms velocity (milli-rad/s)</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell className="font-medium">Analytical</TableCell>
+                    <TableCell>{ar}</TableCell>
+                    <TableCell>{ar}</TableCell>
+                    <TableCell className="text-right">{vr}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell className="font-medium">Experimental</TableCell>
+                    <TableCell>{experimentalAr}</TableCell>
+                    <TableCell>{ar}</TableCell>
+                    <TableCell className="text-right">{experimentalVr}</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
         </>
     )
 }
