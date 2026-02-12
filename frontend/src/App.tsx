12edf7c0689597c3ef3bcae7cb251ae101ff8e85
Miki@0929054164 <milkiyasgebru@gmail.com>
2026-02-12 23:37:37 +0300

    Renamed the title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,15 @@
 import './App.css'
 import InputCard from "@/components/InputCard.tsx";
-import ResultsCard from "@/components/ResultsCard.tsx";
-
-
 function App() {
 
     return (
 
       <div className="bg-[hsl(210,20%,98%)]  min-h-screen w-full py-4 bg-[url('img.png')]">
           <h2 className="text-center max-w-[90%] mx-auto  text-4xl font-bold  bg-white">cloud-based platform for wind response prediction of tall mass timber</h2>
-          <p className="text-center max-w-[80%] mx-auto text-lg mb-4 bg-white">Developed by: McGill Timber Structures Group ( <a className="text-cyan-400 underline" href="https://mcgilltsg.github.io/">McGill TSG</a> )</p>
+          <p className="text-center max-w-[80%] mx-auto text-lg mb-4 bg-white">Developed by: McGill Timber Structures Group ( <a className="text-cyan-400 underline" href="https://mcgilltsg.github.io/" target="_blank">McGill TSG</a> )</p>
           <div className="max-w-1/2  gap-6 mt-3 mb-10 mx-auto">
               <InputCard/>
 
-          </div>
-
-          <div className="max-w-5/6 grid gap-6 mt-3 mb-10 mx-auto">
-              <ResultsCard/>
           </div>
 
       </div>
